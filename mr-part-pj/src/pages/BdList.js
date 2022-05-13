566d5fc211210af7c915850f5699aebe5ca62e57
HyeSPark <80015199+HyeSPark@users.noreply.github.com>
2022-05-13 15:56:22 +0900

    search bd list
    검색기능 완료
    
    todo: 번역 & 예시사진

--- mr-part-pj/src/pages/BdList.js
+++ mr-part-pj/src/pages/BdList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import {Link} from "react-router-dom";
-import {AiFillCaretRight} from "react-icons/ai"
+import {AiFillCaretRight, AiOutlineSearch} from "react-icons/ai"
 
 import useGoogle from "../spreadsheet";
 import NavBar from "../components/NavBar";
@@ -11,6 +11,7 @@ import "../bd-list.css"
 
 function BdList() {
     const res = useGoogle();
+    var rawBdList = res;
     const [bdList, setBdList] = useState();
 
     useEffect(() => {
@@ -74,6 +75,40 @@ function BdList() {
         
     }
 
+    function searchSpace(e) {
+        setBdList(rawBdList.filter(el => el.bd_name.includes(e.target.value)))
+        var els = document.getElementsByClassName('bdList__list')
+        var buttons = document.getElementsByClassName('bdList__direction-arrow')
+        
+        if (e.target.value.length !== 0) {
+            for (let i = 0; i < els.length; i++) {
+                if (els[i].classList.contains("list-hide")) {
+                    if ( i < 3 ) {
+                        buttons[i].classList.add("arrow-rotated")
+                        els[i].classList.remove("list-hide")
+                    } else {
+                        document.getElementsByClassName('bdList__campus-arrow')[0].classList.add("arrow-rotated")
+                        els[i].classList.remove("list-hide")
+                    }
+                };
+            }
+            // 
+            
+        } else {
+            for (let i = 0; i < els.length; i++) {
+                if (!els[i].classList.contains("list-hide")) {
+                    if ( i < 3 ) { 
+                        buttons[i].classList.remove("arrow-rotated")
+                        els[i].classList.add("list-hide")
+                    } else {
+                        document.getElementsByClassName('bdList__campus-arrow')[0].classList.remove("arrow-rotated")
+                        els[i].classList.add("list-hide")
+                    }
+                };
+            }
+        }
+        
+    }
 
 
     return(
@@ -81,6 +116,11 @@ function BdList() {
             <NavBar />
             <div className="bdList__">
                 <h1 className="bdList__title">건물 목록</h1>
+                <div className="bdList__search">
+                    <input type="search" name="q" placeholder="건물 이름을 입력하세요" onChange={(e) => searchSpace(e)} className="bdList__search-input"></input>
+                    <AiOutlineSearch className="bdList__search-icon" />
+                </div>
+                
                 <h2 className="bdList__campus">본교</h2>
                 <h3 className="bdList__direction">동측
                     <button className="bdList__direction-arrow direction-east" onClick={(e) => handleClickArrow(e.target)}><AiFillCaretRight/></button> 
